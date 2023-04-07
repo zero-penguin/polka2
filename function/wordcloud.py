ff0f23c4ab8c9edb96d5da80365cfa947c77ac47
zero-penguin <arato1060@outlook.jp>
2023-04-07 22:18:08 +0900

    fraskの追加とwordcloud出発

---
+++ function/wordcloud.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from collections import defaultdict
+import re
+import io
+import urllib.request
+from zipfile import ZipFile
+
+app = Flask(__name__)
+
+@app.route('/')
+def load_from_url(url):
+    data = urllib.request.urlopen(url).read()
+    zipdata = ZipFile(io.BytesIO(data))
+    filename = zipdata.namelist()[0]
+    text = zipdata.read(filename).decode("shift-jis")
+    text = re.sub(r'［.*?］', '', text)
+    text = re.sub(r'《.*?》', '', text)
+    URL = "https://www.aozora.gr.jp/cards/000119/files/624_ruby_5668.zip"
+    text = load_from_url(URL)
+    view = text.split()[0] 
+    return render_template('index.html', name=name)
+
